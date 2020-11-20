ab1e906b38d5607ecf5146a5b0184c86f3b14f4e
ArtemSydorenko <s1dneyoctane@gmail.com>
2020-11-20 10:41:01 +0200

    added index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from 'react';
+import "./assets/style.css";
+import  ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export default class QuizBee extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="title">QuizBee</div>
+      </div>
+    );
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(<QuizBee />, document.getElementById("root"));
